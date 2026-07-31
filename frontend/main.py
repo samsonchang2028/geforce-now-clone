6bd5a3a6b02e192375df54ce969bfe7f63d04b08
samsonchang2028 <schan119@calpoly.edu>
2026-07-30 21:55:19 -0700

    Add API endpoints for item management and update index.html for item display

--- frontend/main.py
+++ frontend/main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template  # type: ignore
+from flask import Flask, render_template, jsonify, request  # type: ignore
 
 app = Flask(__name__)
+
+
+items = [{"id": 1, "name": "Bob"}, {"id": 2, "name": "Sam"}]
 
 
 @app.route("/")
@@ -16,6 +19,33 @@ def helloWorld():
 @app.route("/page")
 def page():
     return render_template("index.html", title="My Page")
+
+
+@app.route("/api/items", methods=["GET"])
+def get_items():
+    return jsonify(items)
+
+
+@app.route("/api/items/<int:item_id>", methods=["GET"])
+def get_item(item_id):
+    item = None
+    for i in items:
+        if i["id"] == i:
+            item = i
+            break
+    if not item:
+        return jsonify({"error": "item not found"}), 404
+    return jsonify(item)
+
+
+@app.route("/api/items", methods=["POST"])
+def create_item():
+    data = request.get_json()
+    if not data or "name" not in data:
+        return jsonify({"error": "Name is required"}), 400
+    new_item = {"id": len(items) + 1, "name": data["name"]}
+    items.append(new_item)
+    return jsonify(new_item), 201
 
 
 if __name__ == "__main__":
